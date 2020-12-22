0dfd9a9173d0a3ba988cb7098039ca4d60874c77
raymondk <raymond@kth.se>
2020-12-22 10:23:02 +0100

    updated day 22

--- src/util/day_22.rs
+++ src/util/day_22.rs
@@ -1,5 +1,5 @@
 use super::Part;
-use std::collections::{VecDeque, HashSet, HashMap};
+use std::collections::{VecDeque, HashSet};
 use util::Part::{Part2, Part1};
 
 pub fn solve(input : String, part: Part) -> String {
@@ -12,92 +12,100 @@ pub fn solve(input : String, part: Part) -> String {
     result.to_string()
 }
 
-fn parse(input:&str) -> (VecDeque<usize>,VecDeque<usize>) {
-    let mut it = input.split("\n\n");
+#[derive(Hash,Debug,Clone,Eq, PartialEq)]
+struct GameState {
+    player_one:VecDeque<usize>,
+    player_two:VecDeque<usize>,
+}
 
-    let player_one:  VecDeque<usize> = it.next().unwrap().lines()
-        .filter(|s| !s.starts_with("Player"))
-        .map(|s| s.parse().ok().unwrap())
-        .collect();
+impl GameState {
+    fn parse(input:&str) -> GameState {
+        let mut it = input.split("\n\n");
 
-    let player_two:  VecDeque<usize> = it.next().unwrap().lines()
-        .filter(|s| !s.starts_with("Player"))
-        .map(|s| s.parse().ok().unwrap())
-        .collect();
+        let player_one:  VecDeque<usize> = it.next().unwrap().lines()
+            .filter(|s| !s.starts_with("Player"))
+            .map(|s| s.parse().ok().unwrap())
+            .collect();
 
-    (player_one, player_two)
+        let player_two:  VecDeque<usize> = it.next().unwrap().lines()
+            .filter(|s| !s.starts_with("Player"))
+            .map(|s| s.parse().ok().unwrap())
+            .collect();
+
+        GameState{player_one,player_two}
+    }
+
+    fn game_ends(&self) -> bool {
+        self.player_one.is_empty() || self.player_two.is_empty()
+    }
+
+    fn score_player(&self, player:&VecDeque<usize>) -> usize {
+        player.iter().rev().enumerate().map(|(index,card)| *card * (index+1)).sum()
+    }
+
+    fn score(&self) -> usize {
+        match !self.player_one.is_empty() {
+            true => self.score_player(&self.player_one),
+            false => self.score_player(&self.player_two),
+        }
+    }
 }
 
 
-fn play_game(mut player_one:VecDeque<usize>, mut player_two:VecDeque<usize>, mut round:usize, part:Part) -> (bool,usize) {
+fn play_game(mut game_state:GameState, mut _round:usize, part:Part) -> (bool,usize) {
     let mut configutations = HashSet::new();
 
 
-    while !player_one.is_empty() && !player_two.is_empty() {
-        // Is this configuration already played
-        //println!("------------------------\nPlayer one:{:?}, player two:{:?}", player_one, player_two);
+    while !game_state.game_ends() {
 
-        let player_one_before = player_one.clone();
-        let player_two_before = player_two.clone();
+        let game_state_before = game_state.clone();
 
-        let player_one_card = player_one.pop_front().unwrap();
-        let player_two_card = player_two.pop_front().unwrap();
+        let player_one_card = game_state.player_one.pop_front().unwrap();
+        let player_two_card = game_state.player_two.pop_front().unwrap();
 
-        let player_one_wins = if part == Part2 && configutations.contains(&(player_one_before.clone(), player_two_before.clone()))  {
-            //println!("===> Prev Game, player one wins:{}", true);
+        // Is this configuration already played?
+        let player_one_wins = if part == Part2 && configutations.contains(&game_state_before)  {
             // Player one wins
-            true
+            return (true, game_state.score_player(&game_state_before.player_one))
         } else {
 
             // Recursive game
-            if part == Part2 && player_one_card <= player_one.len() && player_two_card <= player_two.len() {
-                //println!("====> Start Recursive game ");
-                let mut player_one_rec = player_one.clone();
-                let mut player_two_rec = player_two.clone();
-                player_one_rec.resize(player_one_card,0);
-                player_two_rec.resize(player_two_card, 0);
+            if part == Part2 && player_one_card <= game_state.player_one.len() && player_two_card <= game_state.player_two.len() {
+                let mut game_state_rec = game_state.clone();
+                game_state_rec.player_one.resize(player_one_card,0);
+                game_state_rec.player_two.resize(player_two_card, 0);
 
-                let (player_one_wins, _) = play_game(player_one_rec, player_two_rec, 1, Part2);
-                //println!("====> Recursive game player one wins:{}", player_one_wins);
+                let (player_one_wins, _) = play_game(game_state_rec, 1, Part2);
 
                 player_one_wins
             } else {
                 // Normal game
                 player_one_card > player_two_card
-                //println!("====> Normal game player one wins:{}", player_one_wins);
-
             }
         };
 
         if player_one_wins {
-            player_one.push_back(player_one_card);
-            player_one.push_back(player_two_card);
+            game_state.player_one.push_back(player_one_card);
+            game_state.player_one.push_back(player_two_card);
         } else {
-            player_two.push_back(player_two_card);
-            player_two.push_back(player_one_card);
+            game_state.player_two.push_back(player_two_card);
+            game_state.player_two.push_back(player_one_card);
         }
 
-        configutations.insert((player_one_before, player_two_before));
-        round += 1;
+        configutations.insert(game_state_before);
+        _round += 1;
     }
 
     // Return (player one wins?, score of winner)
-    if player_two.is_empty() {
-        (true, player_one.iter().rev().enumerate().map(|(index,card)| *card * (index+1)).sum())
-    } else {
-        (false,player_two.iter().rev().enumerate().map(|(index,card)| *card * (index+1)).sum())
-    }
+    (game_state.player_two.is_empty(), game_state.score())
 }
 
 fn part1(input:&str) -> usize {
-    let (player_one,player_two) = parse(input);
-    play_game(player_one, player_two,1, Part1).1
+    play_game(GameState::parse(input),1, Part1).1
 }
 
 fn part2(input:&str) -> usize {
-    let (player_one,player_two) = parse(input);
-    //println!("{:?},{:?}", player_one, player_two);
-    play_game(player_one, player_two, 1, Part2).1
+    play_game(GameState::parse(input),1, Part2).1
 }
 
 
@@ -164,14 +172,14 @@ Player 2:
 29
 14";
 
-        assert_eq!(273,part2(input));
+        assert_eq!(105,part2(input));
     }
 
     //#[test]
     fn test_part2() {
         let input = include_str!("../../input_22.txt");
 
-        assert_eq!(1,part2(input));
+        assert_eq!(35836,part2(input));
     }
 
 
